4cad47736bdfc4a8e79e462f908a079baadcdfdf
serge06 <chusserge@mail.ru>
2025-06-14 00:31:34 +0300

    Ref(03_hammer): Move DPI-C methods to the header file

--- practice/03_hammer/golden/tb/miriscv_scoreboard.sv
+++ practice/03_hammer/golden/tb/miriscv_scoreboard.sv
@@ -1,37 +1,3 @@
-import "DPI-C" function chandle hammer_init(
-  string target_binary
-);
-
-import "DPI-C" function void hammer_release(
-  chandle hammer
-);
-
-import "DPI-C" function void hammer_single_step(
-  chandle hammer
-);
-
-import "DPI-C" function bit [31:0] hammer_get_PC(
-  chandle hammer
-);
-
-import "DPI-C" function void hammer_set_PC(
-  chandle    hammer,
-  bit [31:0] new_pc_value
-);
-
-import "DPI-C" function string hammer_get_insn_str(
-  chandle hammer
-);
-
-import "DPI-C" function bit [31:0] hammer_get_insn_bits(
-  chandle hammer
-);
-
-import "DPI-C" function bit [31:0] hammer_get_gpr(
-  chandle   hammer,
-  bit [4:0] gpr_id
-);
-
 class miriscv_scoreboard;
 
   mailbox#(miriscv_rvfi_item) rvfi_mbx;
