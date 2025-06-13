ed13e3cb3caae4aa9599d4361e34e0e07d56a15e
serge06 <chusserge@mail.ru>
2025-06-14 00:32:20 +0300

    Ref(03_hammer): Remove mem_ prefix from memory model related components

--- practice/03_hammer/golden/tb/miriscv_test.sv
+++ practice/03_hammer/golden/tb/miriscv_test.sv
@@ -3,10 +3,10 @@ class miriscv_test;
             bit               [31:0] signature_addr;
             int unsigned             timeout_in_cycles = 1000000;
 
-            miriscv_mem_agent        mem_slv;
+            miriscv_mem_agent        slv;
             mem_model                mem;
-            miriscv_mem_seq          mem_slv_seq;
-    virtual miriscv_mem_intf         mem_vif;
+            miriscv_mem_seq          slv_seq;
+    virtual miriscv_mem_intf         vif;
 
             miriscv_rvfi_monitor     rvfi_mon;
     virtual miriscv_rvfi_intf        rvfi_vif;
@@ -14,29 +14,29 @@ class miriscv_test;
             miriscv_scoreboard       scb;
 
     function new(
-      virtual miriscv_mem_intf  mem_vif,
+      virtual miriscv_mem_intf  vif,
       virtual miriscv_rvfi_intf rvfi_vif,
     );
-      mailbox#(miriscv_mem_item ) mem_reactive_mbx;
-      mailbox#(miriscv_mem_item ) mem_slv_mbx;
+      mailbox#(miriscv_mem_item ) reactive_mbx;
+      mailbox#(miriscv_mem_item ) slv_mbx;
       mailbox#(miriscv_rvfi_item) rvfi_mbx;
-      this.mem_vif             = mem_vif;
-      mem                      = new();
-      mem_slv                  = new(mem_vif);
-      mem_slv_seq              = new();
-      mem_reactive_mbx         = new();
-      mem_slv_mbx              = new();
-      mem_slv.mon.mbx          = mem_reactive_mbx;
-      mem_slv_seq.reactive_mbx = mem_reactive_mbx;
-      mem_slv_seq.mem          = mem;
-      mem_slv.drv.mbx          = mem_slv_mbx;
-      mem_slv_seq.mbx          = mem_slv_mbx;
-      this.rvfi_vif            = rvfi_vif;
-      rvfi_mon                 = new(rvfi_vif);
-      scb                      = new();
-      rvfi_mbx                 = new();
-      rvfi_mon.mbx             = rvfi_mbx;
-      scb.rvfi_mbx             = rvfi_mbx;
+      this.vif             = vif;
+      slv                  = new(vif);
+      mem                  = new();
+      slv_seq              = new();
+      reactive_mbx         = new();
+      slv_mbx              = new();
+      slv.mon.mbx          = reactive_mbx;
+      slv_seq.reactive_mbx = reactive_mbx;
+      slv_seq.mem          = mem;
+      slv.drv.mbx          = slv_mbx;
+      slv_seq.mbx          = slv_mbx;
+      this.rvfi_vif        = rvfi_vif;
+      rvfi_mon             = new(rvfi_vif);
+      scb                  = new();
+      rvfi_mbx             = new();
+      rvfi_mon.mbx         = rvfi_mbx;
+      scb.rvfi_mbx         = rvfi_mbx;
     endfunction
 
     function void load_binary_to_mem();
@@ -71,7 +71,7 @@ class miriscv_test;
       if($value$plusargs("timeout_in_cycles=%0s", timeout)) begin
         timeout_in_cycles = timeout.atoi();
       end
-      repeat(timeout_in_cycles) mem_vif.wait_clks(1);
+      repeat(timeout_in_cycles) vif.wait_clks(1);
       $display("%0t Test was finished by timeout", $time());
       $display("Amount of retired instructions checked: %0d", scb.get_retire_cnt());
       $finish();
@@ -93,13 +93,13 @@ class miriscv_test;
 
     virtual task wait_for_test_done();
       miriscv_mem_item t = new();
-      wait(mem_vif.arst_n === 1);
+      wait(vif.arst_n === 1);
       forever begin
-        mem_vif.wait_clks(1);
-        mem_slv.mon.get_data(t);
+        vif.wait_clks(1);
+        slv.mon.get_data(t);
         if(test_done_cond(t) == 1) break;
       end
-      repeat(10) mem_vif.wait_clks(1);
+      repeat(10) vif.wait_clks(1);
       $display("Test end condition was detected. Test done!");
       $display("Amount of retired instructions checked: %0d", scb.get_retire_cnt());
       $finish();
@@ -110,8 +110,8 @@ class miriscv_test;
       load_binary_to_mem();
       hammer_init();
       fork
-        mem_slv.run();
-        mem_slv_seq.run();
+        slv.run();
+        slv_seq.run();
         rvfi_mon.run();
         scb.run();
         timeout();
